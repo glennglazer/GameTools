e4ee63756b717f32ab935f5035844e01d670f094
glennglazer <glenn.glazer@gmail.com>
2022-12-26 10:17:36 -0800

    Better arg names

--- Morrowind/enchanting/items_parse/morrowind_parse_enchant_csv_to_json.py
+++ Morrowind/enchanting/items_parse/morrowind_parse_enchant_csv_to_json.py
@@ -21,38 +21,38 @@ def write_file(parsed: list, outfile: str) -> bool:
     with open(outfile, mode='w') as of:
         json.dump(parsed, of)
         
-def check_for_files(inDir: str) -> bool:
+def check_for_files(in_dir: str) -> bool:
     rv = True
     for prefix in FILE_PREFIXES:
-        rv = rv and op.exists(inDir + '/' + prefix + '.csv')
+        rv = rv and op.exists(in_dir + '/' + prefix + '.csv')
     return rv
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("inDir", help="absolute path to directory to read CSV files from")
-    parser.add_argument("outDir", nargs='?', help="absolute path to directory to (over) write enchanting JSON to. Defaults to inDir.")
+    parser.add_argument("in_dir", help="absolute path to directory to read CSV files from")
+    parser.add_argument("out_dir", nargs='?', help="absolute path to directory to (over) write enchanting JSON to. Defaults to in_dir.")
     args = parser.parse_args()
-    inDir = args.inDir
-    outDir = args.outDir
+    in_dir = args.in_dir
+    out_dir = args.out_dir
     
-    if not inDir or not op.exists(args.inDir):
-        print(f"Read directory not given or does not exist: {inDir}")
+    if not in_dir or not op.exists(args.in_dir):
+        print(f"Read directory not given or does not exist: {in_dir}")
         parser.print_usage()
         sys.exit(1)
-    elif not check_for_files(inDir):
-        print(f"One or more of {FILE_PREFIXES} CSV files missing from specified input directory {inDir}")
+    elif not check_for_files(in_dir):
+        print(f"One or more of {FILE_PREFIXES} CSV files missing from specified input directory {in_dir}")
     
-    if not outDir:
-        outDir = inDir
+    if not out_dir:
+        out_dir = in_dir
     else:
-        if not op.exists(outDir):
-            print(f"Specified output directory does not exist: {outDir}")
+        if not op.exists(out_dir):
+            print(f"Specified output directory does not exist: {out_dir}")
             parser.print_usage()
             sys.exit(1)
     
     for item_type in FILE_PREFIXES:
-        item_read = f"{inDir}/{item_type}.csv"
-        item_write = f"{outDir}/{item_type}.json"
+        item_read = f"{in_dir}/{item_type}.csv"
+        item_write = f"{out_dir}/{item_type}.json"
         item_list = []
         
         # Given the arg testing, we are guaranteed these exist. Format, validity, etc. is not.
