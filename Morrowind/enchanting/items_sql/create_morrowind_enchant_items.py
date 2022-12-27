cc964b0339986791a569d53c15aed83440de5b4a
glennglazer <glenn.glazer@gmail.com>
2022-12-27 10:51:20 -0800

    add enchanting effects

--- Morrowind/enchanting/items_sql/create_morrowind_enchant_items.py
+++ Morrowind/enchanting/items_sql/create_morrowind_enchant_items.py
@@ -13,7 +13,7 @@ import pandas as pd
 import sqlite3
 import sys
 
-FILE_PREFIXES = ['armor', 'books', 'clothing', 'weapons', 'soul_gems']
+FILE_PREFIXES = ['armor', 'books', 'clothing', 'weapons', 'soul_gems', 'magic_effects']
 
 def check_for_files(in_dir: str) -> bool:
     rv = True
